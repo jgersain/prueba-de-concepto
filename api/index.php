fd1311306144bbde41e08b7a2456065df29b2da1
jgersain <jhersein@gmail.com>
2020-05-31 20:28:25 -0500

    Add backend entrypoint

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, false, false);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $payload = json_encode(['hello' => 'world'], JSON_PRETTY_PRINT);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
